075424545073f778f5d1fbecefc175cd4cc843a5
Djl <2860351416@qq.com>
2019-03-25 19:55:39 +0800

    6

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/about','StaticPagesController@about')->name('about');
 Route::get('signup','UsersController@create')->name('signup');
 //严格按照了 RESTful 架构对路由进行设计
 Route::resource('users','UsersController');
+//显示登录页面
+Route::get('login', 'SessionsController@create')->name('login');
+//创建新会话（登录）
+Route::post('login', 'SessionsController@store')->name('login');
+//销毁会话（退出登录）        
+Route::delete('logout', 'SessionsController@destroy')->name('logout');
